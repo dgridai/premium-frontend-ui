e11a29683b606bf78d4a5bc788ae39cd443ba420
metaarch888 <haifun455@gmail.com>
2025-09-28 15:53:48 +0800

    modify page

---
+++ dist/assets/all-wallets-BCmS75Ch.js
@@ -0,0 +1,6 @@
+import{b as l}from"./core-CjJW35j2.js";import"./index-BMgUA7v9.js";import"./index.es-CT0FeqWP.js";const i=l`<svg fill="none" viewBox="0 0 24 24">
+  <path
+    style="fill: var(--wui-color-accent-100);"
+    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
+  />
+</svg>`;export{i as allWalletsSvg};
