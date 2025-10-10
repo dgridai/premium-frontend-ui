5a19b425e05ed482fd19e0989a618cc54ce0658f
metaarch888 <haifun455@gmail.com>
2025-10-10 10:38:26 +0800

    chore: modify fonts

--- dist/assets/cursor-DB9DQHgm.js
+++
@@ -1,3 +0,0 @@
-import{b as o}from"./core-BbwUJDZ1.js";import"./index-CcWFfl6X.js";import"./index.es-BnOua0S4.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{l as cursorSvg};
