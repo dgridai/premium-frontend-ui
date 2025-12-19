52f74d8b698471d548fdabd1bf9b7ff6e1def872
metaarch888 <haifun455@gmail.com>
2025-12-19 15:37:11 +0800

    chore: modify api

---
+++ dist/assets/cursor-CMB6asJG.js
@@ -0,0 +1,3 @@
+import{b as o}from"./core-SzqqI0Qq.js";import"./index-Dx5aCHVB.js";import"./index.es-REubdo-X.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
