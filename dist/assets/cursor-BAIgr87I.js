3aa90974acfbf4b6d0cd379b6445860094993578
metaarch888 <haifun455@gmail.com>
2025-12-26 13:48:51 +0800

    chore: modify page

--- dist/assets/cursor-BAIgr87I.js
+++
@@ -1,3 +0,0 @@
-import{b as o}from"./core-CE-ZRuXB.js";import"./index-qpw6qug9.js";import"./index.es-wou5FJ-7.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{l as cursorSvg};
