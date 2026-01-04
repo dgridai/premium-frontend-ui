c6210065ed468cab0a382b2bfab05a2c8c3b3342
metaarch888 <haifun455@gmail.com>
2026-01-04 17:58:17 +0800

    chore: modify airdrop page

---
+++ dist/assets/cursor-DlPui_f9.js
@@ -0,0 +1,3 @@
+import{b as o}from"./core-D1w9qLPt.js";import"./index-Bhq9VjbY.js";import"./index.es-BHc0yrKQ.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
