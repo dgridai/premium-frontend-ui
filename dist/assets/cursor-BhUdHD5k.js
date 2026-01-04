5ef9ecc6057cdc5823b30159aa7d5fd00d0aba91
metaarch888 <haifun455@gmail.com>
2026-01-04 17:35:49 +0800

    chore: modify language  txt

--- dist/assets/cursor-BhUdHD5k.js
+++
@@ -1,3 +0,0 @@
-import{b as o}from"./core-DdgG__S7.js";import"./index-C816-JG9.js";import"./index.es-_VZB0AYd.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{l as cursorSvg};
