3c014fa415046122dc8a08f9d7a4b92618591f5c
metaarch888 <haifun455@gmail.com>
2026-02-17 20:15:21 +0800

    chore: modify reward page

--- dist/assets/chevron-bottom-Cq4KE2xo.js
+++
@@ -1,8 +0,0 @@
-import{b as o}from"./core-8DCc1Vlo.js";import"./index-q0yca_N9.js";import"./index.es-C5-41byJ.js";const t=o`<svg fill="none" viewBox="0 0 16 16">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M1.46 4.96a1 1 0 0 1 1.41 0L8 10.09l5.13-5.13a1 1 0 1 1 1.41 1.41l-5.83 5.84a1 1 0 0 1-1.42 0L1.46 6.37a1 1 0 0 1 0-1.41Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{t as chevronBottomSvg};
