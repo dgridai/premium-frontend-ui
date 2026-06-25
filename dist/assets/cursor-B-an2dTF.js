4dc833bcc8030d097a0b2ffff1b6552679213ba5
metaarch888 <haifun455@gmail.com>
2026-06-25 20:48:26 +0800

    chore: modify reward card style

---
+++ dist/assets/cursor-B-an2dTF.js
@@ -0,0 +1,3 @@
+import{b as o}from"./core-BsQUQSgT.js";import"./index-_L2EGekB.js";import"./index.es-D0DURMRT.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
