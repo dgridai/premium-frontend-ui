4bb00d97dd151ff89797396bc48ae354e199fb76
metaarch888 <haifun455@gmail.com>
2026-08-18 01:06:46 +0800

    chore: modify header style

---
+++ dist/assets/cursor-DsHb7tDq.js
@@ -0,0 +1,3 @@
+import{b as o}from"./core-DOYhdA4_.js";import"./index-Bj8wSDTE.js";import"./index.es-Bv6C4zZj.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
