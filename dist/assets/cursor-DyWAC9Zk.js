bf52865a9e8ce19732953cde2280779b81f3e902
metaarch888 <haifun455@gmail.com>
2026-08-25 16:42:26 +0800

    chore: modify rewards page button style

--- dist/assets/cursor-DyWAC9Zk.js
+++
@@ -1,3 +0,0 @@
-import{b as o}from"./core-D0K9sVC7.js";import"./index-CeLse4Vo.js";import"./index.es-Dmm24h4D.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{l as cursorSvg};
---
+++ dist/assets/cursor-fR2X5k65.js
@@ -0,0 +1,3 @@
+import{b as o}from"./core-BKe57fXL.js";import"./index-vKE6P8cR.js";import"./index.es-D13fbXt6.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
