cef1b8d7f9fb64eefcb42e661c5d749bcc1a62bb
metaarch888 <haifun455@gmail.com>
2026-08-25 15:53:22 +0800

    chore: modify reward page style

---
+++ dist/assets/index-Dfh3S6Jb.js
@@ -0,0 +1,1 @@
+import{r as u}from"./index-CeLse4Vo.js";import{u as f,i as c}from"./Pagination-FFio6TFf.js";var l=function(n,r,e){e===void 0&&(e={});var a=f(n),t=u.useRef(null),i=u.useCallback(function(){t.current&&clearInterval(t.current)},[]);return u.useEffect(function(){if(!(!c(r)||r<0))return e.immediate&&a(),t.current=setInterval(a,r),i},[r,e.immediate]),i};export{l as u};
